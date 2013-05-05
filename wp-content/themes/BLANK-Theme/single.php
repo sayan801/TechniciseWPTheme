09eeaa14c05b94c7142bc7877e2335a1af9a8861
SANJIT PAUL <sanjit.cn@gmail.com>
2013-05-05 19:23:55 +0530

    created a custom theme
    created a custom theme (not completed)

---
+++ wp-content/themes/BLANK-Theme/single.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div>
+			
+			<h2><?php the_title(); ?></h2>
+			
+			<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+
+			<div class="entry">
+				
+				<?php the_content(); ?>
+
+				<?php //wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+			</div>
+			
+			<?php edit_post_link('Edit this entry','','.'); ?>
+			
+		</div>
+
+<?php comments_template(); ?>
+
+	<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
